f248e81b1721832a2636e7d00d16c4fc7e4e738f
Saniya Licorish <slicorish05@gmail.com>
2025-01-10 00:58:26 -0500

    Create LoginRegister.js

---
+++ src/components/login-register/LoginRegister.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginRegister() {
+    return (
+        <div>
+            <h1>Login/Register</h1>
+            {/* Add your login/register form here */}
+        </div>
+    );
+}
+
+export default LoginRegister
