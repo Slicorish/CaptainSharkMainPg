3b960c8df14707892ce0e86b7cb5d4f44225b20a
Saniya Licorish <slicorish05@gmail.com>
2025-01-09 20:34:35 -0500

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,39 +1,42 @@
 import React, {useState} from 'react'
-import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
 
 import './NavbarStyles.css'
 
 function Navbar(){
     const[nav, setNav] = useState(false)
-    const handleNav = () => setNav(!nav)
+    const handleNav = () => {
+        setNav(!nav)
+        console.log(nav)
+    }
     return(
-        <div className = "navbar">
-            <div className = "logo">
+        <div className='navbar'>
+            <div className="logo">
                 <h2>CAPTAINSHARK</h2>
-        </div>
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>Login/Register</li>
-            <li>CaptainShark</li>
+            </div>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>Login/Register</li>
+                <li>CaptainShark</li>
             </ul>
             <div className='hamburger' onClick={handleNav}>
                 <HiOutlineMenuAlt4 className='icon' />
-                </div>
-                <div className={nav ? 'mobile-nav active' : 'mobiile-nav'}>
-                    <ul className='mobile-nav'>
-                        <li>Home</li>
-                        <li>Login/Register</li>
-                        <li>CaptainShark</li>
-                    </ul>
-                    <div className='mobile-menu-bottom'>
-                        <div className='menu-icons'>
-                            <button>Search</button>     {/*can change later*/}
-                            <button>Account</button>     {/*can change later*/}
-                            </div>
-                        </div>
-                    </div>
             </div>
+            <div className={nav ? 'mobile-menu active' : 'mobiile-menu'}>
+                <ul className='mobile-nav'>
+                    <li>Home</li>
+                    <li>Login/Register</li>
+                    <li>CaptainShark</li>
+                </ul>
+                <div className='mobile-menu-bottom'>
+                    <div className='menu-icons'>
+                        <button>Search</button>     {/*can change later*/}
+                        <button>Account</button>     {/*can change later*/}
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
-
 export default Navbar
