5a11e7e953440352a7fe285c62d007747e471898
Saniya Licorish <slicorish05@gmail.com>
2025-01-10 01:12:36 -0500

    Create CaptainShark.js

---
+++ src/components/captainshark/CaptainShark.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CaptainSharkStyles.css';
+
+function CaptainShark() {
+    return (
+        <div className="captain-shark">
+            <h1>CaptainShark</h1>
+            {/* Add your login/register form here */}
+        </div>
+    );
+}
+
+export default CaptainShark
