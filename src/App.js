487eb74e5df9f6f5774ba4d3a7a454316ade14ef
Saniya Licorish <slicorish05@gmail.com>
2025-01-10 00:58:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import LoginRegister from './components/login-register/LoginRegister';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/login-register" element={<LoginRegister />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
